c3c421220e3a9cbf0a29cafcd655930c2f364e4b
Liu Jiajun <iamljj@gmail.com>
2019-11-29 14:51:59 +0800

    update at 2019-11-29 14:51:59

---
+++ pub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# build pages
+gitbook build
+
+# check if there is any error
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+# push to github
+git add .
+git commit -m "update at $(date '+%Y-%m-%d %H:%M:%S')"
+git push origin master
